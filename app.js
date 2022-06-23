73b8ef2a10513ec0a8715ae0ef1ebc3ebb31928d
nancycrojas <nanclarisaok@gmail.com>
2022-06-23 09:36:29 -0300

    Variables, datos y operadores - ejercicio 11

---
+++ app.js
@@ -0,0 +1,9 @@
+//Perímetro de un rectángulo
+
+//Crear un programa que pida al usuario ingresar el valor de la altura y el valor del ancho de un rectángulo, calcular su perímetro y mostrarlo en un mensaje.
+
+
+const altura = prompt ("Por favor ingresa  el valor de la altura del rectángulo");
+const ancho = prompt ("Por favor ingresa el valor de la ancho del rectángulo");
+
+alert("El perímetro del rectángulo es: " + (2 * altura + 2 * ancho));
