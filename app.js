22bc823350947873fdbd59d7b080f9fcbca5827f
nancycrojas <nanclarisaok@gmail.com>
2022-09-29 21:37:12 -0300

    Agregando  variable y template strings

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 
 //Crear un programa que pida al usuario ingresar el valor de la altura y el valor del ancho de un rectángulo, calcular su perímetro y mostrarlo en un mensaje.
 
+const altura = Number(prompt("Por favor ingresa el valor de la altura del rectángulo"));
+const ancho = Number(prompt("Por favor ingresa el valor del ancho del rectángulo"));
 
-const altura = prompt ("Por favor ingresa  el valor de la altura del rectángulo");
-const ancho = prompt ("Por favor ingresa el valor de la ancho del rectángulo");
+//alert("El perímetro del rectángulo es: " + (2 * altura + 2 * ancho));
 
-alert("El perímetro del rectángulo es: " + (2 * altura + 2 * ancho));
+const perimetroRectangulo = (2 * (altura + ancho));
+alert(`El perímetro del rectángulo de altura ${altura} y ancho ${ancho} es: ${perimetroRectangulo}.`);
